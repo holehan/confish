327b84e57ac1610ba6c93e55e2507c089e242c60
Florian Grässle <hallo@holehan.org>
2019-11-01 20:13:18 +0100

    Add aliases

---
+++ functions/gti.fish
@@ -0,0 +1,4 @@
+# Defined in - @ line 1
+function gti --description 'alias gti=git'
+	git  $argv;
+end
---
+++ functions/wget.fish
@@ -0,0 +1,4 @@
+# Defined in - @ line 1
+function wget --description 'alias wget=wget -c'
+	command wget -c $argv;
+end
