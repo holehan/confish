58d793854f27da994273a1550f672dee81055aad
Florian Grässle <hallo@holehan.org>
2019-11-06 12:57:09 +0100

    Add colors

---
+++ conf.d/colors.fish
@@ -0,0 +1,22 @@
+set -g fish_color_autosuggestion BD93F9
+set -g fish_color_cancel -r
+set -g fish_color_command F8F8F2
+set -g fish_color_comment 779174
+set -g fish_color_cwd green
+set -g fish_color_cwd_root red
+set -g fish_color_end 50FA7B
+set -g fish_color_error FFB86C
+set -g fish_color_escape 00a6b2
+set -g fish_color_history_current --bold
+set -g fish_color_host normal
+set -g fish_color_match --background=brblue
+set -g fish_color_normal normal
+set -g fish_color_operator 00a6b2
+set -g fish_color_param FF79C6
+set -g fish_color_quote F1FA8C
+set -g fish_color_redirection 8BE9FD
+set -g fish_color_search_match bryellow --background=brblack
+set -g fish_color_selection white --bold --background=brblack
+set -g fish_color_status red
+set -g fish_color_user brgreen
+set -g fish_color_valid_path --underline
